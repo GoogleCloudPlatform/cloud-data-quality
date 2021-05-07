adb66c0d6c89e489454ae4c4803047e86b9870fd
Thinh Ha <thinh.t.ha@gmail.com>
2021-05-07 11:47:06 +0000

    simplify utility scripts

--- scripts/bazel_test.sh
+++ scripts/bazel_test.sh
@@ -13,27 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -o errexit
-set -o nounset
-set -o pipefail
+#set -o errexit
+#set -o nounset
+#set -o pipefail
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # shellcheck source=/dev/null
 source "$ROOT/scripts/common.sh"
 
 function bazel_test() {
-  confirm_gcloud_login || err "Failed to retrieve gcloud application-default credentials."
-  GOOGLE_SDK_CREDENTIALS="${GOOGLE_SDK_CREDENTIALS}"
-  GOOGLE_CLOUD_PROJECT="${GOOGLE_CLOUD_PROJECT}"
+  confirm_gcloud_login || (( err "Failed to retrieve gcloud application-default credentials." && exit 1 ))
+  if [[ -z "${GOOGLE_CLOUD_PROJECT:-}" ]]; then
+    err "Environment variable GOOGLE_CLOUD_PROJECT not found."
+    local GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
+    if [[ -z "${GOOGLE_CLOUD_PROJECT}" ]]; then
+      err "Unable to find gcloud project config."
+      exit 1
+    fi
+    err "Temporarily setting GOOGLE_CLOUD_PROJECT to: ${GOOGLE_CLOUD_PROJECT}"
+  fi
+  if [[ -z "${GOOGLE_SDK_CREDENTIALS:-}" ]]; then
+    err "Environment variable GOOGLE_SDK_CREDENTIALS not found."
+    if [[ -f ~/.config/gcloud/application_default_credentials.json ]]; then
+       local GOOGLE_SDK_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
+    else
+      err "Unable to find gcloud application-default credentials at location '~/.config/gcloud/application_default_credentials.json'."
+      exit 1
+    fi
+    err "Temporarily setting GOOGLE_SDK_CREDENTIALS to: ${GOOGLE_SDK_CREDENTIALS}"
+  fi
   set -x
   bin/bazelisk test \
-    --test_env GOOGLE_CLOUD_PROJECT="$GOOGLE_CLOUD_PROJECT" \
-    --test_env GOOGLE_APPLICATION_CREDENTIALS="$GOOGLE_SDK_CREDENTIALS" \
-    --test_timeout 600 \
-    --test_output errors \
-    --test_summary detailed \
-    --test_verbose_timeout_warnings \
-    //test"s${1:-/...}"
+    --test_env GOOGLE_CLOUD_PROJECT="${GOOGLE_CLOUD_PROJECT}" \
+    --test_env GOOGLE_APPLICATION_CREDENTIALS="${GOOGLE_SDK_CREDENTIALS}" \
+    //tests"${1:-/...}"
   set +x
 }
 
