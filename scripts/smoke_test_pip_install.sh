1cb01fa30aae844ecddebff3fa9c4621acabec73
Thinh Ha <thinh.t.ha@gmail.com>
2021-06-07 16:48:25 +0000

    grr
    Signed-off-by: Thinh Ha <thinh.t.ha@gmail.com>

--- scripts/smoke_test_pip_install.sh
+++ scripts/smoke_test_pip_install.sh
@@ -27,6 +27,8 @@ python3 -m pip --version
 # make sure wheel is installed
 pip install wheel --upgrade --force
 pip install setuptools --upgrade --force
+python3 -m pip install wheel --upgrade --force
+python3 -m pip install setuptools --upgrade --force
 python3 -c "import setuptools; print(setuptools.__version__)"
 
 # install poetry
