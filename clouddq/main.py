0f4dca4b4af06c8b95c2e6f6aee972d99616471e
Thinh Ha <thinh.t.ha@gmail.com>
2021-09-29 14:24:38 +0000

    use coloredlogs

--- clouddq/main.py
+++ clouddq/main.py
@@ -13,17 +13,19 @@
 # limitations under the License.
 
 """Data Quality Engine for BigQuery."""
+from datetime import datetime
 import json
-from pathlib import Path
-from pprint import pprint
-import typing
 import logging
 import logging.config
+from pathlib import Path
+from pprint import pprint
+import subprocess
 import sys
 import traceback
+import typing
 
 import click
-import click_logging
+import coloredlogs
 
 from clouddq import lib
 from clouddq import utils
@@ -45,7 +47,11 @@ def not_null_or_empty(
 
 
 APP_NAME = "clouddq"
-APP_VERSION = "git rev-parse HEAD"
+APP_VERSION = (
+    subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+    .decode("ascii")
+    .strip()
+)
 LOG_LEVEL = logging._nameToLevel["INFO"]
 
 
@@ -67,71 +73,65 @@ class JsonEncoderDatetime(JsonEncoderStrFallback):
             return super().default(obj)
 
 
-logger = logging.getLogger("clouddq")
-logging.basicConfig(
-    format="%(json_formatted)s",
-    level=LOG_LEVEL,
-    handlers=[
-        logging.FileHandler(Path('clouddq.log'), 'a'),
-        logging.StreamHandler(sys.stderr),
-    ],
-)
-_record_factory_bak = logging.getLogRecordFactory()
+class JSONFormatter(logging.Formatter):
+    def __init__(self):
+        super().__init__()
 
-
-def record_factory(*args, **kwargs) -> logging.LogRecord:
-    record = _record_factory_bak(*args, **kwargs)
-    record.json_formatted = json.dumps(
-        {
-            "severity": record.levelname,
-            "time": record.created,
-            "location": "{}:{}:{}".format(
-                record.pathname or record.filename,
-                record.funcName,
-                record.lineno,
-            ),
-            "exception": record.exc_info,
-            "traceback": traceback.format_exception(*record.exc_info)
-            if record.exc_info
-            else None,
-            "message": record.getMessage(),
-            "labels": {
-                "name": APP_NAME,
-                "releaseId": APP_VERSION,
+    def format(self, record):
+        record.msg = json.dumps(
+            {
+                "severity": record.levelname,
+                "time": datetime.utcfromtimestamp(record.created)
+                .astimezone()
+                .isoformat()
+                .replace("+00:00", "Z"),
+                "logging.googleapis.com/sourceLocation": {
+                    "file": record.pathname or record.filename,
+                    "function": record.funcName,
+                    "line": record.lineno,
+                },
+                "exception": record.exc_info,
+                "traceback": traceback.format_exception(*record.exc_info)
+                if record.exc_info
+                else None,
+                "message": record.getMessage(),
+                "logging.googleapis.com/labels": {
+                    "name": APP_NAME,
+                    "releaseId": APP_VERSION,
+                },
             },
-        },
-        cls=JsonEncoderDatetime,
+            cls=JsonEncoderDatetime,
+        )
+        return super().format(record)
+
+
+def get_json_logger():
+    json_logger = logging.getLogger("clouddq-json-logger")
+    json_logger.setLevel(LOG_LEVEL)
+    logging_stream_handler = logging.StreamHandler(sys.stderr)
+    logging_stream_handler.setFormatter(JSONFormatter())
+    json_logger.addHandler(logging_stream_handler)
+    return json_logger
+
+
+def get_logger():
+    logger = logging.getLogger("clouddq")
+    logger.setLevel(LOG_LEVEL)
+    logging_stream_handler = logging.StreamHandler(sys.stderr)
+    stream_formatter = logging.Formatter(
+        "{asctime} {name} {levelname:8s} {message}", style="{"
     )
-    return record
+    logging_stream_handler.setFormatter(stream_formatter)
+    logger.addHandler(logging_stream_handler)
+    return logger
 
 
-logging.setLogRecordFactory(record_factory)
+json_logger = get_json_logger()
+logger = get_logger()
+coloredlogs.install(logger=logger)
 
-click_logging_style_kwargs = {
-    "debug": dict(fg="cyan", blink=True),
-    "info": dict(fg="yellow", blink=True),
-    "warn": dict(fg="magenta", blink=True),
-    "error": dict(fg="red", blink=True),
-    "exception": dict(fg="red", blink=True),
-    "critical": dict(fg="red", blink=True),
-    }
-click_logging_echo_kwargs = {
-    "debug": dict(err=True),
-    "info": dict(err=True),
-    "warn": dict(err=True),
-    "error": dict(err=True),
-    "exception": dict(err=True),
-    "critical": dict(err=True),
-    }
-click_logging.basic_config(
-    logger,
-    style_kwargs = click_logging_style_kwargs,
-    echo_kwargs = click_logging_echo_kwargs,
-
-)
 
 @click.command()
-@click_logging.simple_verbosity_option(logger)
 @click.argument("rule_binding_ids")
 @click.argument(
     "rule_binding_config_path",
@@ -161,7 +161,7 @@ click_logging.basic_config(
     "e.g. dev, test, prod.  "
     "Defaults 'dev' or the environment variable ENV if set.",
     envvar="ENV",
-    default="dev",
+    default="clouddq",
     callback=not_null_or_empty,
 )
 @click.option(
@@ -245,127 +245,131 @@ def main(  # noqa: C901
       --environment_target=dev
 
     """
-
     if debug:
         logger.setLevel("DEBUG")
-    logger.debug("Current working directory: %s", Path().cwd())
-    dbt_profiles_dir = Path(dbt_profiles_dir).absolute()
-    if not dbt_profiles_dir.joinpath("profiles.yml").is_file():
-        raise ValueError(
-            f"Cannot find connection `profiles.yml` configurations at "
-            f"`dbt_profiles_dir` path: {dbt_profiles_dir}"
-        )
-    logger.debug("Using 'dbt_profiles_dir': %s", dbt_profiles_dir)
-    if not dbt_path:
-        dbt_path = Path().cwd().joinpath("dbt").absolute()
-        logger.debug(
-            "No argument 'dbt_path' provided. Defaulting to use "
-            "'dbt' directory in current working directory at: %s",
-            dbt_path,
-        )
-    else:
-        dbt_path = Path(dbt_path).absolute()
-        if dbt_path.name != "dbt":
-            dbt_path = dbt_path / "dbt"
-    if not dbt_path.is_dir():
-        logger.debug("Creating a new dbt directory at 'dbt_path': %s", dbt_path)
-        dbt_path.mkdir(parents=True, exist_ok=True)
-    logger.info("Using 'dbt_path': %s", dbt_path)
-    dbt_project_path = dbt_path.absolute().joinpath("dbt_project.yml")
-    if not dbt_project_path.is_file():
-        logger.debug(
-            f"Cannot find `dbt_project.yml` configurations in current path: {dbt_project_path}"
-            f"Writing templated 'dbt_project.yml' to: {dbt_project_path}"
-        )
-        dbt_project_path.write_text(get_template_file(Path("dbt", "dbt_project.yml")))
-    logger.info("Using 'dbt_project_path': %s", dbt_project_path)
-    dbt_main_path = dbt_path / "models" / "data_quality_engine"
-    dbt_main_path.mkdir(parents=True, exist_ok=True)
-    if not dbt_main_path.joinpath("main.sql").is_file():
-        dbt_main_path.joinpath("main.sql").write_text(
-            get_template_file(
-                Path("dbt").joinpath("models", "data_quality_engine", "main.sql")
-            )
-        )
-    if not dbt_main_path.joinpath("dq_summary.sql").is_file():
-        dbt_main_path.joinpath("dq_summary.sql").write_text(
-            get_template_file(
-                Path("dbt").joinpath("models", "data_quality_engine", "dq_summary.sql")
-            )
-        )
-    dbt_rule_binding_views_path = dbt_path / "models" / "rule_binding_views"
-    dbt_rule_binding_views_path.mkdir(parents=True, exist_ok=True)
-    logger.info(
-                "Writing generated sql to %s/",
-                dbt_rule_binding_views_path.absolute(),
-            )
-    configs_path = Path(rule_binding_config_path)
-    logger.debug("Loading rule bindings from: %s", configs_path.absolute())
-    metadata = json.loads(metadata)
-    dq_summary_table_name = lib.get_bigquery_dq_summary_table_name(
-        dbt_profiles_dir, environment_target, dbt_project_path
-    )
-    logger.info(
-        "Writing summary results to GCP table: `%s`. ",
-        dq_summary_table_name,
-    )
-    all_rule_bindings = lib.load_rule_bindings_config(Path(configs_path))
-    target_rule_binding_ids = [r.strip() for r in rule_binding_ids.split(",")]
-    if len(target_rule_binding_ids) == 1 and target_rule_binding_ids[0] == "ALL":
-        target_rule_binding_ids = list(all_rule_bindings.keys())
-    (
-        entities_collection,
-        row_filters_collection,
-        rules_collection,
-    ) = lib.load_configs_if_not_defined(
-        configs_path=configs_path,
-    )
-    for rule_binding_id in target_rule_binding_ids:
-        rule_binding_configs = all_rule_bindings.get(rule_binding_id, None)
-        utils.assert_not_none_or_empty(
-            rule_binding_configs,
-            f"Target Rule Binding Id: {rule_binding_id} not found "
-            f"in config path {configs_path.absolute()}.",
-        )
-        if debug:
-            logger.debug(
-                "Creating sql string from configs for rule binding: %s",
-                rule_binding_id,
-            )
-            logger.debug("Rule binding config json:")
-            pprint(rule_binding_configs)
-        sql_string = lib.create_rule_binding_view_model(
-            rule_binding_id=rule_binding_id,
-            rule_binding_configs=rule_binding_configs,
-            dq_summary_table_name=dq_summary_table_name,
-            entities_collection=entities_collection,
-            rules_collection=rules_collection,
-            row_filters_collection=row_filters_collection,
-            configs_path=configs_path,
-            environment=environment_target,
-            metadata=metadata,
-            debug=print_sql_queries,
-            progress_watermark=progress_watermark,
-        )
-        if not skip_sql_validation:
-            validate_sql_string(sql_string)
-        if debug:
-            logger.debug(
-                "*** Writing sql to {dbt_rule_binding_views_path.absolute()}/" "%s.sql",
-                rule_binding_id,
-            )
-        lib.write_sql_string_as_dbt_model(
-            rule_binding_id=rule_binding_id,
-            sql_string=sql_string,
-            dbt_rule_binding_views_path=dbt_rule_binding_views_path,
-        )
-    # clean up old rule_bindings
-    for view in dbt_rule_binding_views_path.glob("*.sql"):
-        if view.stem not in target_rule_binding_ids:
-            view.unlink()
-    configs = {"target_rule_binding_ids": target_rule_binding_ids}
+    logger.info("Starting CloudDQ run with parameters:")
+    json_logger.warn(locals())
     try:
-        logger.info("Running dbt in path: %s", dbt_path)
+        logger.debug("Current working directory: %s", Path().cwd())
+        dbt_profiles_dir = Path(dbt_profiles_dir).absolute()
+        if not dbt_profiles_dir.joinpath("profiles.yml").is_file():
+            raise ValueError(
+                f"Cannot find connection `profiles.yml` configurations at "
+                f"`dbt_profiles_dir` path: {dbt_profiles_dir}"
+            )
+        logger.debug("Using 'dbt_profiles_dir': %s", dbt_profiles_dir)
+        if not dbt_path:
+            dbt_path = Path().cwd().joinpath("dbt").absolute()
+            logger.debug(
+                "No argument 'dbt_path' provided. Defaulting to use "
+                "'dbt' directory in current working directory at: %s",
+                dbt_path,
+            )
+        else:
+            dbt_path = Path(dbt_path).absolute()
+            if dbt_path.name != "dbt":
+                dbt_path = dbt_path / "dbt"
+        if not dbt_path.is_dir():
+            logger.debug(f"Creating a new dbt directory at 'dbt_path': {dbt_path}")
+            dbt_path.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Using 'dbt_path': {dbt_path}")
+        dbt_project_path = dbt_path.absolute().joinpath("dbt_project.yml")
+        if not dbt_project_path.is_file():
+            logger.debug(
+                f"Cannot find `dbt_project.yml` configurations in current path: "
+                f"{dbt_project_path}.\n"
+                f"Writing templated 'dbt_project.yml' to: {dbt_project_path}"
+            )
+            dbt_project_path.write_text(
+                get_template_file(Path("dbt", "dbt_project.yml"))
+            )
+        logger.info(f"Using 'dbt_project_path': {dbt_project_path}")
+        dbt_main_path = dbt_path / "models" / "data_quality_engine"
+        dbt_main_path.mkdir(parents=True, exist_ok=True)
+        if not dbt_main_path.joinpath("main.sql").is_file():
+            dbt_main_path.joinpath("main.sql").write_text(
+                get_template_file(
+                    Path("dbt").joinpath("models", "data_quality_engine", "main.sql")
+                )
+            )
+        if not dbt_main_path.joinpath("dq_summary.sql").is_file():
+            dbt_main_path.joinpath("dq_summary.sql").write_text(
+                get_template_file(
+                    Path("dbt").joinpath(
+                        "models", "data_quality_engine", "dq_summary.sql"
+                    )
+                )
+            )
+        dbt_rule_binding_views_path = dbt_path / "models" / "rule_binding_views"
+        dbt_rule_binding_views_path.mkdir(parents=True, exist_ok=True)
+        logger.info(
+            f"Writing generated sql to {dbt_rule_binding_views_path.absolute()}/"
+        )
+        configs_path = Path(rule_binding_config_path)
+        logger.debug(f"Loading rule bindings from: {configs_path.absolute()}")
+        metadata = json.loads(metadata)
+        dq_summary_table_name = lib.get_bigquery_dq_summary_table_name(
+            dbt_profiles_dir, environment_target, dbt_project_path
+        )
+        logger.info(
+            f"Writing summary results to GCP table: `{dq_summary_table_name}`. "
+        )
+        all_rule_bindings = lib.load_rule_bindings_config(Path(configs_path))
+        target_rule_binding_ids = [r.strip() for r in rule_binding_ids.split(",")]
+        if len(target_rule_binding_ids) == 1 and target_rule_binding_ids[0] == "ALL":
+            target_rule_binding_ids = list(all_rule_bindings.keys())
+        (
+            entities_collection,
+            row_filters_collection,
+            rules_collection,
+        ) = lib.load_configs_if_not_defined(
+            configs_path=configs_path,
+        )
+        for rule_binding_id in target_rule_binding_ids:
+            rule_binding_configs = all_rule_bindings.get(rule_binding_id, None)
+            utils.assert_not_none_or_empty(
+                rule_binding_configs,
+                f"Target Rule Binding Id: {rule_binding_id} not found "
+                f"in config path {configs_path.absolute()}.",
+            )
+            if debug:
+                logger.debug(
+                    f"Creating sql string from configs for rule binding: "
+                    f"{rule_binding_id}"
+                )
+                logger.debug("Rule binding config json:")
+                pprint(rule_binding_configs)
+            sql_string = lib.create_rule_binding_view_model(
+                rule_binding_id=rule_binding_id,
+                rule_binding_configs=rule_binding_configs,
+                dq_summary_table_name=dq_summary_table_name,
+                entities_collection=entities_collection,
+                rules_collection=rules_collection,
+                row_filters_collection=row_filters_collection,
+                configs_path=configs_path,
+                environment=environment_target,
+                metadata=metadata,
+                debug=print_sql_queries,
+                progress_watermark=progress_watermark,
+            )
+            if not skip_sql_validation:
+                validate_sql_string(sql_string)
+            if debug:
+                logger.debug(
+                    f"*** Writing sql to {dbt_rule_binding_views_path.absolute()}/"
+                    f"{rule_binding_id}.sql",
+                )
+            lib.write_sql_string_as_dbt_model(
+                rule_binding_id=rule_binding_id,
+                sql_string=sql_string,
+                dbt_rule_binding_views_path=dbt_rule_binding_views_path,
+            )
+        # clean up old rule_bindings
+        for view in dbt_rule_binding_views_path.glob("*.sql"):
+            if view.stem not in target_rule_binding_ids:
+                view.unlink()
+        configs = {"target_rule_binding_ids": target_rule_binding_ids}
+        logger.info(f"Running dbt in path: {dbt_path}")
         utils.run_dbt(
             dbt_path=dbt_path,
             dbt_profile_dir=dbt_profiles_dir,
@@ -374,8 +378,9 @@ def main(  # noqa: C901
             debug=debug,
             dry_run=dry_run,
         )
-    except Exception as e:
-        logger.error("Encountered unexpected error: " + str(e), exc_info=True)
+    except Exception as error:
+        json_logger.error(error)
+        logger.exception(error, exc_info=True)
 
 
 if __name__ == "__main__":
