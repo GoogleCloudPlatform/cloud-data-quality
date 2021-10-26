479a2a2727b451a867f0dbf3cf225392ed8c7c1b
Thinh Ha <thinh.t.ha@gmail.com>
2021-10-26 07:45:43 +0000

    fix env vars

--- set_environment_variables.sh
+++ set_environment_variables.sh
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export GOOGLE_CLOUD_PROJECT=kthxbayes-sandbox
-export CLOUDDQ_BIGQUERY_DATASET=dq_test
-export CLOUDDQ_BIGQUERY_REGION=EU
-export GOOGLE_SDK_CREDENTIALS=/home/thinhha_google_com/projects/cloud-data-quality/kthxbayes-sandbox-795b00ff6b67.json
-export IMPERSONATION_SERVICE_ACCOUNT=argo-svc@kthxbayes-sandbox.iam.gserviceaccount.com
+export GOOGLE_CLOUD_PROJECT="<your-project-id>"
+export CLOUDDQ_BIGQUERY_DATASET="<your-bigquery-dataset-id>"
+export CLOUDDQ_BIGQUERY_REGION="<gcp-region-for-bigquery-jobs>"
+export GOOGLE_SDK_CREDENTIALS="<path-to-exported-service-account-key>"
+export IMPERSONATION_SERVICE_ACCOUNT="<service-account-name-for-impersonation>"
