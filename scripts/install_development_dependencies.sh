9f943b43f5c82a1875ab9d8b559f7bf48a3605c3
Thinh Ha <thinh.t.ha@gmail.com>
2021-11-09 09:29:42 +0000

    install deps

--- scripts/install_development_dependencies.sh
+++ scripts/install_development_dependencies.sh
@@ -29,7 +29,9 @@ fi
 
 # Install Python dependencies
 DEBIAN_FRONTEND=noninteractive sudo apt-get update && sudo apt-get install -y \
-make build-essential  \
+make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
 zip unzip python3-pip python3-venv
 
 # Install sandboxfs
