c7ffe88d06ed513688c01a608332e4559dd6f66a
Thinh Ha <thinh.t.ha@gmail.com>
2021-11-30 22:22:33 +0000

    fix broken logic

--- clouddq/templates/dbt/models/data_quality_engine/main.sql
+++ clouddq/templates/dbt/models/data_quality_engine/main.sql
@@ -29,6 +29,7 @@
         configs_hashsum,
         dataplex_lake,
         dataplex_zone,
+        dataplex_asset_id,
         dq_run_id,
         progress_watermark,
         rows_validated,
@@ -59,31 +60,8 @@
     FROM
         {{ ref(rule_binding_id) }}
     GROUP BY
-        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
     {% if loop.nextitem is defined %}
     UNION ALL
     {% endif %}
-{% else %}
-    SELECT
-        NULL as invocation_id,
-        NULL AS execution_ts,
-        NULL AS rule_binding_id,
-        NULL AS rule_id,
-        NULL AS table_id,
-        NULL AS column_id,
-        NULL AS metadata_json_string,
-        NULL AS configs_hashsum,
-        NULL AS dq_run_id,
-        NULL AS progress_watermark,
-        NULL AS rows_validated,
-        NULL AS complex_rule_validation_errors_count,
-        NULL AS last_modified,
-        NULL AS success_count,
-        NULL AS success_percentage,
-        NULL AS failed_count,
-        NULL AS failed_percentage,
-        NULL AS null_count,
-        NULL AS null_percentage,
-        NULL AS entity_configs
-    LIMIT 0
 {%- endfor -%}
