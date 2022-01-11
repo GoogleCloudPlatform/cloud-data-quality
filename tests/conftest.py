a7f1a1c2f7f38819226128333459c72ac0a4cc70
AmandeepSinghCS <amandeep.singh@cloudsufi.com>
2022-01-11 09:24:03 +0000

    added test for spark dq rules validation

--- tests/conftest.py
+++ tests/conftest.py
@@ -258,6 +258,10 @@ def source_dq_rules_configs_file_path():
     return Path("tests").joinpath("resources").joinpath("dq_rules_configs.yml").absolute()
 
 @pytest.fixture(scope="session")
+def source_spark_dq_rules_configs_file_path():
+    return Path("tests").joinpath("resources").joinpath("spark_dq_rules_configs.yml").absolute()
+
+@pytest.fixture(scope="session")
 def test_profiles_dir():
     return Path("tests").joinpath("resources", "test_dbt_profiles_dir").absolute()
 
@@ -388,6 +392,33 @@ def temp_configs_from_dq_rules_config_file(
     temp_clouddq_dir = Path(tmp_path).joinpath("clouddq_test_dq_rules_configs")
     # Copy over tests/resources/configs
     registry_defaults = shutil.copyfile(source_dq_rules_configs_file_path, temp_clouddq_dir)
+    # Prepare entity_uri configs
+    with open(registry_defaults) as source_file:
+        lines = source_file.read()
+    with open(registry_defaults, "w") as source_file:
+        lines = lines.replace("<my-gcp-dataplex-lake-id>", gcp_dataplex_lake_name)
+        lines = lines.replace("<my-gcp-dataplex-region-id>", gcp_dataplex_region)
+        lines = lines.replace("<my-gcp-project-id>", gcp_project_id)
+        lines = lines.replace("<my-gcp-dataplex-zone-id>", gcp_dataplex_zone_id)
+        lines = lines.replace("<my_bigquery_dataset_id>", gcp_dataplex_bigquery_dataset_id)
+        source_file.write(lines)
+    yield temp_clouddq_dir.absolute()
+    if os.path.exists(temp_clouddq_dir):
+        os.unlink(temp_clouddq_dir)
+
+@pytest.fixture(scope="function")
+def temp_spark_configs_from_dq_rules_config_file(
+        gcp_project_id,
+        gcp_dataplex_bigquery_dataset_id,
+        gcp_dataplex_region,
+        gcp_dataplex_lake_name,
+        gcp_dataplex_zone_id,
+        source_spark_dq_rules_configs_file_path,
+        tmp_path):
+    # Create temp directory
+    temp_clouddq_dir = Path(tmp_path).joinpath("clouddq_spark_test_dq_rules_configs")
+    # Copy over tests/resources/configs
+    registry_defaults = shutil.copyfile(source_spark_dq_rules_configs_file_path, temp_clouddq_dir)
     # Prepare entity_uri configs
     with open(registry_defaults) as source_file:
         lines = source_file.read()
--- tests/integration/test_dq_rules.py
+++ tests/integration/test_dq_rules.py
@@ -69,6 +69,34 @@ class TestDqRules:
         logger.info(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
         return table_id
 
+    @pytest.fixture(scope="session")
+    def create_spark_expected_results_table(
+        self,
+        client,
+        test_resources,
+        gcp_project_id,
+        target_bq_result_dataset_name,
+        target_bq_result_table_name,):
+
+        client = client.get_connection()
+
+        table_id = f"{gcp_project_id}.{target_bq_result_dataset_name}.spark_{target_bq_result_table_name}_expected"
+
+        job_config = bigquery.LoadJobConfig(
+            source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+            autodetect=True,
+            write_disposition="WRITE_TRUNCATE")
+
+        with open(test_resources / "spark_dq_rules_expected_results.json", "rb") as source_file:
+            json_data = json.loads(source_file.read())
+            job = client.load_table_from_json(json_data, table_id, job_config=job_config)
+
+        job.result()  # Waits for the job to complete.
+
+        table = client.get_table(table_id)  # Make an API request.
+        logger.info(f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {table_id}")
+        return table_id
+
     def test_dq_rules(
         self,
         runner,
@@ -158,7 +186,7 @@ class TestDqRules:
                         logger.info(lines)
                     logger.warning(
                         "Rows with values not matching the expected "
-                        "content in 'tests/resources/expected_results.csv':"
+                        "content in 'tests/resources/expected_results.json':"
                     )
                     for row in rows:
                         record = json.loads(str(row[0]))
@@ -167,6 +195,117 @@ class TestDqRules:
                 failed_rows_rule_binding_ids = [row['rule_binding_id'] for row in failed_rows]
                 failed_rows_rule_ids = [row['rule_id'] for row in failed_rows]
                 with open(test_resources / "dq_rules_expected_results.json", "rb") as source_file:
+                    expected_json = []
+                    json_data = json.loads(source_file.read())
+                    for record in json_data:
+                        if record['rule_binding_id'] not in failed_rows_rule_binding_ids:
+                            continue
+                        if record['rule_id'] not in failed_rows_rule_ids:
+                            continue
+                        expected_json.append(record)
+                assert failed_rows == expected_json
+        finally:
+            shutil.rmtree(temp_dir)
+
+    def test_spark_dq_rules(
+        self,
+        runner,
+        temp_spark_configs_from_dq_rules_config_file,
+        gcp_application_credentials,
+        gcp_project_id,
+        gcp_bq_dataset,
+        gcp_bq_region,
+        target_bq_result_dataset_name,
+        target_bq_result_table_name,
+        tmp_path,
+        client,
+        gcp_impersonation_credentials,
+        gcp_sa_key,
+        create_spark_expected_results_table,
+        source_spark_dq_rules_configs_file_path,
+        test_resources,
+        caplog,
+    ):
+        caplog.set_level(logging.INFO, logger="clouddq")
+        try:
+            temp_dir = Path(tmp_path).joinpath("clouddq_spark_test_dq_rules")
+            temp_dir.mkdir(parents=True)
+            with working_directory(temp_dir):
+                logger.info(f"test_last_modified_in_dq_summary {gcp_application_credentials}")
+                target_table = f"{gcp_project_id}.{target_bq_result_dataset_name}.{target_bq_result_table_name}"
+                args = [
+                    "ALL",
+                    f"{temp_spark_configs_from_dq_rules_config_file}",
+                    f"--gcp_project_id={gcp_project_id}",
+                    f"--gcp_bq_dataset_id={gcp_bq_dataset}",
+                    f"--gcp_region_id={gcp_bq_region}",
+                    f"--target_bigquery_summary_table={target_table}",
+                    f"--print_sql_queries --debug"
+                    ]
+                result = runner.invoke(main, args)
+                assert result.exit_code == 0
+
+                # Prepare dbt runtime
+                dbt_runner = DbtRunner(
+                    dbt_path=None,
+                    dbt_profiles_dir=None,
+                    environment_target="spark_thrift",
+                    gcp_project_id=gcp_project_id,
+                    gcp_region_id=gcp_bq_region,
+                    gcp_bq_dataset_id=gcp_bq_dataset,
+                    gcp_service_account_key_path=gcp_sa_key,
+                    gcp_impersonation_credentials=gcp_impersonation_credentials,
+                    spark_runner=True,
+                )
+                dbt_path = dbt_runner.get_dbt_path(spark_runner=True)
+                invocation_id = get_dbt_invocation_id(dbt_path)
+                logger.info(f"Spark Dbt invocation id is: {invocation_id}")
+                # Test the DQ expected results
+                sql = f"""
+                WITH validation_errors AS (
+                SELECT rule_binding_id, rule_id, column_id,
+                dimension, metadata_json_string, progress_watermark,
+                rows_validated, complex_rule_validation_errors_count,
+                complex_rule_validation_success_flag,
+                success_count, failed_count, null_count
+                FROM
+                    `{gcp_project_id}.{target_bq_result_dataset_name}.{target_bq_result_table_name}`
+                WHERE
+                    invocation_id='{invocation_id}'
+                EXCEPT DISTINCT
+                SELECT rule_binding_id, rule_id, column_id,
+                dimension, metadata_json_string, progress_watermark,
+                rows_validated, complex_rule_validation_errors_count,
+                complex_rule_validation_success_flag,
+                success_count, failed_count, null_count
+                FROM
+                    `{create_spark_expected_results_table}`
+                )
+                SELECT TO_JSON_STRING(validation_errors)
+                FROM validation_errors;
+                """
+                logger.info(f"SQL query is: {sql}")
+                query_job = client.execute_query(sql)
+                results = query_job.result()
+                logger.info("Query done")
+                rows = list(results)
+                logger.info(f"Query execution returned {len(rows)} rows")
+                if len(rows):
+                    logger.info(f"Input yaml from {source_spark_dq_rules_configs_file_path}:")
+                    with open(source_spark_dq_rules_configs_file_path) as input_yaml:
+                        lines = input_yaml.read()
+                        logger.info(lines)
+                    logger.warning(
+                        "Rows with values not matching the expected "
+                        "content in 'tests/resources/spark_expected_results.json':"
+                    )
+                    for row in rows:
+                        record = json.loads(str(row[0]))
+                        logger.warning(f"\n{pformat(record)}")
+                failed_rows = [json.loads(row[0]) for row in rows]
+                failed_rows_rule_binding_ids = [row['rule_binding_id'] for row in failed_rows]
+                failed_rows_rule_ids = [row['rule_id'] for row in failed_rows]
+                with open(test_resources / "spark_dq_rules_expected_results.json", "rb") as source_file:
                     expected_json = []
                     json_data = json.loads(source_file.read())
                     for record in json_data:
