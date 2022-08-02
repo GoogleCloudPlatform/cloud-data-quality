5fec7b749878c163a8afbec425d377e59fe9bbe1
AmandeepSinghCS <amandeep.singh@cloudsufi.com>
2022-08-02 11:59:38 +0000

    added column_value to failed_records query

--- clouddq/templates/dbt/macros/failed_records_query.sql
+++ clouddq/templates/dbt/macros/failed_records_query.sql
@@ -86,6 +86,7 @@ all_validation_results AS (
     r.rule_binding_id AS rule_binding_id,
     r.rule_id AS rule_id,
     r.column_id AS column_id,
+    r.column_value as column_value,
     CAST(r.dimension AS STRING) AS dimension,
     r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
     r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
