6c3ac40b2c59f5ab69f4e4b4df8fd8b4873ba683
AmandeepSinghCS <amandeep.singh@cloudsufi.com>
2022-08-09 09:13:12 +0000

    formatted sql statement

--- tests/resources/bq_native_partitioned_sql_expected.sql
+++ tests/resources/bq_native_partitioned_sql_expected.sql
@@ -13,287 +13,306 @@
 -- limitations under the License.
 
 WITH
-zero_record AS (
+  zero_record AS (
+  SELECT
+    '<rule_binding_id>' AS rule_binding_id,
+    ),
+  DATA AS (
+  SELECT
+    *,
+    '<rule_binding_id>' AS rule_binding_id,
+  FROM
+    `<your-gcp-project-id>.austin_311.contact_details_partitioned` d
+  WHERE
+    contact_type = 'email'
+    AND ts IS NOT NULL),
+  last_mod AS (
+  SELECT
+    project_id || '.' || dataset_id || '.' || table_id AS table_id,
+    TIMESTAMP_MILLIS(last_modified_time) AS last_modified
+  FROM
+    `<your-gcp-project-id>.austin_311.__TABLES__` ),
+  validation_results AS (
+  SELECT
+    CURRENT_TIMESTAMP() AS execution_ts,
+    '<rule_binding_id>' AS rule_binding_id,
+    'NO_DUPLICATES_IN_COLUMN_GROUPS' AS rule_id,
+    '<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
+    CAST(NULL AS STRING) AS column_id,
+    NULL AS column_value,
+    custom_sql_statement_validation_errors.row_id AS row_id,
+    custom_sql_statement_validation_errors.contact_type AS contact_type,
+    custom_sql_statement_validation_errors.value AS value,
+    CAST(NULL AS STRING) AS dimension,
+    CAST(NULL AS BOOLEAN) AS simple_rule_row_is_valid,
+    TRUE AS skip_null_count,
+    custom_sql_statement_validation_errors.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+    CASE
+      WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
+      WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count = 0 THEN TRUE
+    ELSE
+    FALSE
+  END
+    AS complex_rule_validation_success_flag,
+    """
+  WITH
+    zero_record AS (
+    SELECT
+      '<rule_binding_id>' AS rule_binding_id,
+      ),
+    DATA AS (
+    SELECT
+      *,
+      '<rule_binding_id>' AS rule_binding_id,
+    FROM
+      `<your-gcp-project-id>.austin_311.contact_details_partitioned` d
+    WHERE
+      contact_type = 'email'
+      AND ts IS NOT NULL),
+    last_mod AS (
+    SELECT
+      project_id || '.' || dataset_id || '.' || table_id AS table_id,
+      TIMESTAMP_MILLIS(last_modified_time) AS last_modified
+    FROM
+      `<your-gcp-project-id>.austin_311.__TABLES__` ),
+    validation_results AS (
+    SELECT
+      CURRENT_TIMESTAMP() AS execution_ts,
+      '<rule_binding_id>' AS rule_binding_id,
+      'NO_DUPLICATES_IN_COLUMN_GROUPS' AS rule_id,
+      '<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
+      CAST(NULL AS STRING) AS column_id,
+      NULL AS column_value,
+      custom_sql_statement_validation_errors.row_id AS row_id,
+      custom_sql_statement_validation_errors.contact_type AS contact_type,
+      custom_sql_statement_validation_errors.value AS value,
+      CAST(NULL AS STRING) AS dimension,
+      CAST(NULL AS BOOLEAN) AS simple_rule_row_is_valid,
+      TRUE AS skip_null_count,
+      custom_sql_statement_validation_errors.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+      CASE
+        WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
+        WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count = 0 THEN TRUE
+      ELSE
+      FALSE
+    END
+      AS complex_rule_validation_success_flag,
+    FROM
+      zero_record
+    LEFT JOIN (
+      SELECT
+        *,
+        '<rule_binding_id>' AS _rule_binding_id,
+        COUNT(*) OVER() AS complex_rule_validation_errors_count,
+      FROM (
+        SELECT
+          a.*
+        FROM
+          DATA a
+        INNER JOIN (
+          SELECT
+            contact_type,
+            value
+          FROM
+            DATA
+          GROUP BY
+            contact_type,
+            value
+          HAVING
+            COUNT(*) > 1 ) duplicates
+        USING
+          (contact_type,
+            value) ) custom_sql ) custom_sql_statement_validation_errors
+    ON
+      zero_record.rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id ),
+    all_validation_results AS (
+    SELECT
+      r.rule_binding_id AS rule_binding_id,
+      r.rule_id AS rule_id,
+      r.column_id AS column_id,
+      r.column_value AS column_value,
+      CAST(r.dimension AS STRING) AS dimension,
+      r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
+      r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+      r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
+      r.row_id AS row_id,
+      r.contact_type AS contact_type,
+      r.value AS value,
+    FROM
+      validation_results r )
+  SELECT
+    *
+  FROM
+    all_validation_results
+  WHERE
+    simple_rule_row_is_valid IS FALSE
+    OR complex_rule_validation_success_flag IS FALSE
+  ORDER BY
+    rule_id""" AS failed_records_query,
+  FROM
+    zero_record
+  LEFT JOIN (
+    SELECT
+      *,
+      '<rule_binding_id>' AS _rule_binding_id,
+      COUNT(*) OVER() AS complex_rule_validation_errors_count,
+    FROM (
+      SELECT
+        a.*
+      FROM
+        DATA a
+      INNER JOIN (
+        SELECT
+          contact_type,
+          value
+        FROM
+          DATA
+        GROUP BY
+          contact_type,
+          value
+        HAVING
+          COUNT(*) > 1 ) duplicates
+      USING
+        (contact_type,
+          value) ) custom_sql ) custom_sql_statement_validation_errors
+  ON
+    zero_record.rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id
+  UNION ALL
+  SELECT
+    CURRENT_TIMESTAMP() AS execution_ts,
+    '<rule_binding_id>' AS rule_binding_id,
+    'NOT_NULL_SIMPLE' AS rule_id,
+    '<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
+    'value' AS column_id,
+    DATA.value AS column_value,
+    DATA.row_id AS row_id,
+    DATA.contact_type AS contact_type,
+    DATA.value AS value,
+    CAST(NULL AS STRING) AS dimension,
+    CASE
+      WHEN value IS NOT NULL THEN TRUE
+    ELSE
+    FALSE
+  END
+    AS simple_rule_row_is_valid,
+    TRUE AS skip_null_count,
+    CAST(NULL AS INT64) AS complex_rule_validation_errors_count,
+    CAST(NULL AS BOOLEAN) AS complex_rule_validation_success_flag,
+    """
+  WITH
+    zero_record AS (
+    SELECT
+      '<rule_binding_id>' AS rule_binding_id,
+      ),
+    DATA AS (
+    SELECT
+      *,
+      '<rule_binding_id>' AS rule_binding_id,
+    FROM
+      `<your-gcp-project-id>.austin_311.contact_details_partitioned` d
+    WHERE
+      contact_type = 'email'
+      AND ts IS NOT NULL),
+    last_mod AS (
+    SELECT
+      project_id || '.' || dataset_id || '.' || table_id AS table_id,
+      TIMESTAMP_MILLIS(last_modified_time) AS last_modified
+    FROM
+      `<your-gcp-project-id>.austin_311.__TABLES__` ),
+    validation_results AS (
+    SELECT
+      CURRENT_TIMESTAMP() AS execution_ts,
+      '<rule_binding_id>' AS rule_binding_id,
+      'NOT_NULL_SIMPLE' AS rule_id,
+      '<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
+      'value' AS column_id,
+      DATA.value AS column_value,
+      DATA.row_id AS row_id,
+      DATA.contact_type AS contact_type,
+      DATA.value AS value,
+      CAST(NULL AS STRING) AS dimension,
+      CASE
+        WHEN value IS NOT NULL THEN TRUE
+      ELSE
+      FALSE
+    END
+      AS simple_rule_row_is_valid,
+      TRUE AS skip_null_count,
+      CAST(NULL AS INT64) AS complex_rule_validation_errors_count,
+      CAST(NULL AS BOOLEAN) AS complex_rule_validation_success_flag,
+    FROM
+      zero_record
+    LEFT JOIN
+      DATA
+    ON
+      zero_record.rule_binding_id = DATA.rule_binding_id ),
+    all_validation_results AS (
+    SELECT
+      r.rule_binding_id AS rule_binding_id,
+      r.rule_id AS rule_id,
+      r.column_id AS column_id,
+      r.column_value AS column_value,
+      CAST(r.dimension AS STRING) AS dimension,
+      r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
+      r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+      r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
+      r.row_id AS row_id,
+      r.contact_type AS contact_type,
+      r.value AS value,
+    FROM
+      validation_results r )
+  SELECT
+    *
+  FROM
+    all_validation_results
+  WHERE
+    simple_rule_row_is_valid IS FALSE
+    OR complex_rule_validation_success_flag IS FALSE
+  ORDER BY
+    rule_id""" AS failed_records_query,
+  FROM
+    zero_record
+  LEFT JOIN
+    DATA
+  ON
+    zero_record.rule_binding_id = DATA.rule_binding_id ),
+  all_validation_results AS (
+  SELECT
+    r.execution_ts AS execution_ts,
+    r.rule_binding_id AS rule_binding_id,
+    r.rule_id AS rule_id,
+    r.table_id AS table_id,
+    r.column_id AS column_id,
+    r.column_value AS column_value,
+    CAST(r.dimension AS STRING) AS dimension,
+    r.skip_null_count AS skip_null_count,
+    r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
+    r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+    r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
+    r.row_id AS row_id,
+    r.contact_type AS contact_type,
+    r.value AS value,
+    (
+    SELECT
+      COUNT(*)
+    FROM
+      DATA) AS rows_validated,
+    last_mod.last_modified,
+    '{"brand": "one"}' AS metadata_json_string,
+    '' AS configs_hashsum,
+    CAST(NULL AS STRING) AS dataplex_lake,
+    CAST(NULL AS STRING) AS dataplex_zone,
+    CAST(NULL AS STRING) AS dataplex_asset_id,
+    CONCAT(r.rule_binding_id, '_', r.rule_id, '_', r.execution_ts, '_', TRUE) AS dq_run_id,
+    TRUE AS progress_watermark,
+    failed_records_query AS failed_records_query,
+  FROM
+    validation_results r
+  JOIN
+    last_mod
+  USING
+    (table_id) )
 SELECT
-'<rule_binding_id>' AS rule_binding_id,
-),
-data AS (
-SELECT
-*,
-'<rule_binding_id>' AS rule_binding_id,
+  *
 FROM
-`<your-gcp-project-id>.austin_311.contact_details_partitioned` d
-WHERE
-contact_type = 'email'
-AND ts IS NOT NULL),
-last_mod AS (
-SELECT
-project_id || '.' || dataset_id || '.' || table_id AS table_id,
-TIMESTAMP_MILLIS(last_modified_time) AS last_modified
-FROM `<your-gcp-project-id>.austin_311.__TABLES__`
-),
-validation_results AS (
-SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NO_DUPLICATES_IN_COLUMN_GROUPS' AS rule_id,
-'<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
-CAST(NULL AS STRING) AS column_id,
-NULL AS column_value,
-custom_sql_statement_validation_errors.row_id AS row_id,
-custom_sql_statement_validation_errors.contact_type AS contact_type,
-custom_sql_statement_validation_errors.value AS value,
-CAST(NULL AS STRING) AS dimension,
-CAST(NULL AS BOOLEAN) AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-custom_sql_statement_validation_errors.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-CASE
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count = 0 THEN TRUE
-ELSE FALSE
-END AS complex_rule_validation_success_flag,
-"""
-WITH
-zero_record AS (
-SELECT
-'<rule_binding_id>' AS rule_binding_id,
-),
-data AS (
-SELECT
-*,
-'<rule_binding_id>' AS rule_binding_id,
-FROM
-`<your-gcp-project-id>.austin_311.contact_details_partitioned` d
-WHERE
-contact_type = 'email'
-AND ts IS NOT NULL),
-last_mod AS (
-SELECT
-project_id || '.' || dataset_id || '.' || table_id AS table_id,
-TIMESTAMP_MILLIS(last_modified_time) AS last_modified
-FROM `<your-gcp-project-id>.austin_311.__TABLES__`
-),
-validation_results AS (SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NO_DUPLICATES_IN_COLUMN_GROUPS' AS rule_id,
-'<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
-CAST(NULL AS STRING) AS column_id,
-NULL AS column_value,
-custom_sql_statement_validation_errors.row_id AS row_id,
-custom_sql_statement_validation_errors.contact_type AS contact_type,
-custom_sql_statement_validation_errors.value AS value,
-CAST(NULL AS STRING) AS dimension,
-CAST(NULL AS BOOLEAN) AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-custom_sql_statement_validation_errors.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-CASE
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count = 0 THEN TRUE
-ELSE FALSE
-END AS complex_rule_validation_success_flag,
-FROM
-zero_record
-LEFT JOIN
-(
-SELECT
-*,
-'<rule_binding_id>' AS _rule_binding_id,
-COUNT(*) OVER() AS complex_rule_validation_errors_count,
-FROM (
-select a.*
-from data a
-inner join (
-select
-contact_type,value
-from data
-group by contact_type,value
-having count(*) > 1
-) duplicates
-using (contact_type,value)
-) custom_sql
-) custom_sql_statement_validation_errors
-ON
-zero_record.rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id
-),
-all_validation_results AS (
-SELECT
-r.rule_binding_id AS rule_binding_id,
-r.rule_id AS rule_id,
-r.column_id AS column_id,
-r.column_value AS column_value,
-CAST(r.dimension AS STRING) AS dimension,
-r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
-r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
-r.row_id AS row_id,
-r.contact_type AS contact_type,
-r.value AS value,FROM
-validation_results r
-)
-SELECT
-*
-FROM
-all_validation_results
-WHERE
-simple_rule_row_is_valid is False
-OR
-complex_rule_validation_success_flag is False
-ORDER BY rule_id"""
-AS failed_records_query,
-FROM
-zero_record
-LEFT JOIN
-(
-SELECT
-*,
-'<rule_binding_id>' AS _rule_binding_id,
-COUNT(*) OVER() AS complex_rule_validation_errors_count,
-FROM (
-select a.*
-from data a
-inner join (
-select
-contact_type,value
-from data
-group by contact_type,value
-having count(*) > 1
-) duplicates
-using (contact_type,value)
-) custom_sql
-) custom_sql_statement_validation_errors
-ON
-zero_record.rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id
-UNION ALL
-SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NOT_NULL_SIMPLE' AS rule_id,
-'<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
-'value' AS column_id,
-data.value AS column_value,
-data.row_id AS row_id,
-data.contact_type AS contact_type,
-data.value AS value,
-CAST(NULL AS STRING) AS dimension,
-CASE
-WHEN value IS NOT NULL THEN TRUE
-ELSE
-FALSE
-END AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-CAST(NULL AS INT64) AS complex_rule_validation_errors_count,
-CAST(NULL AS BOOLEAN) AS complex_rule_validation_success_flag,
-"""
-WITH
-zero_record AS (
-SELECT
-'<rule_binding_id>' AS rule_binding_id,
-),
-data AS (
-SELECT
-*,
-'<rule_binding_id>' AS rule_binding_id,
-FROM
-`<your-gcp-project-id>.austin_311.contact_details_partitioned` d
-WHERE
-contact_type = 'email'
-AND ts IS NOT NULL),
-last_mod AS (
-SELECT
-project_id || '.' || dataset_id || '.' || table_id AS table_id,
-TIMESTAMP_MILLIS(last_modified_time) AS last_modified
-FROM `<your-gcp-project-id>.austin_311.__TABLES__`
-),
-validation_results AS (SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NOT_NULL_SIMPLE' AS rule_id,
-'<your-gcp-project-id>.austin_311.contact_details_partitioned' AS table_id,
-'value' AS column_id,
-data.value AS column_value,
-data.row_id AS row_id,
-data.contact_type AS contact_type,
-data.value AS value,
-CAST(NULL AS STRING) AS dimension,
-CASE
-WHEN value IS NOT NULL THEN TRUE
-ELSE
-FALSE
-END AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-CAST(NULL AS INT64) AS complex_rule_validation_errors_count,
-CAST(NULL AS BOOLEAN) AS complex_rule_validation_success_flag,
-FROM
-zero_record
-LEFT JOIN
-data
-ON
-zero_record.rule_binding_id = data.rule_binding_id
-),
-all_validation_results AS (
-SELECT
-r.rule_binding_id AS rule_binding_id,
-r.rule_id AS rule_id,
-r.column_id AS column_id,
-r.column_value AS column_value,
-CAST(r.dimension AS STRING) AS dimension,
-r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
-r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
-r.row_id AS row_id,
-r.contact_type AS contact_type,
-r.value AS value,FROM
-validation_results r
-)
-SELECT
-*
-FROM
-all_validation_results
-WHERE
-simple_rule_row_is_valid is False
-OR
-complex_rule_validation_success_flag is False
-ORDER BY rule_id"""
-AS failed_records_query,
-FROM
-zero_record
-LEFT JOIN
-data
-ON
-zero_record.rule_binding_id = data.rule_binding_id
-),
-all_validation_results AS (
-SELECT
-r.execution_ts AS execution_ts,
-r.rule_binding_id AS rule_binding_id,
-r.rule_id AS rule_id,
-r.table_id AS table_id,
-r.column_id AS column_id,
-r.column_value AS column_value,
-CAST(r.dimension AS STRING) AS dimension,
-r.skip_null_count AS skip_null_count,
-r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
-r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
-r.row_id AS row_id,
-r.contact_type AS contact_type,
-r.value AS value,
-(SELECT COUNT(*) FROM data) AS rows_validated,
-last_mod.last_modified,
-'{"brand": "one"}' AS metadata_json_string,
-'' AS configs_hashsum,
-CAST(NULL AS STRING) AS dataplex_lake,
-CAST(NULL AS STRING) AS dataplex_zone,
-CAST(NULL AS STRING) AS dataplex_asset_id,
-CONCAT(r.rule_binding_id, '_', r.rule_id, '_', r.execution_ts, '_', True) AS dq_run_id,
-TRUE AS progress_watermark,
-failed_records_query AS failed_records_query,
-FROM
-validation_results r
-JOIN last_mod USING(table_id)
-)
-SELECT
-*
-FROM
-all_validation_results
+  all_validation_results
