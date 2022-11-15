33ba944f6bc3084b295cd7c9da9da92156009dec
shourya116 <shourya.singh@cloudsufi.com>
2022-11-15 07:53:09 +0000

    testing-CLA

--- clouddq/main.py
+++ clouddq/main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Data Quality Engine for BigQuery."""
+"""Testing CLA"""
 from datetime import datetime
 from datetime import timezone
 from pathlib import Path
