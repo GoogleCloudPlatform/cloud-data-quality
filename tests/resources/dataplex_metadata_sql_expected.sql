45b6693d3218b0a79de7cdf7edde2597bef8d310
shourya116 <shourya.singh@cloudsufi.com>
2023-01-25 13:00:50 +0000

    fixed ambiguous columns

--- tests/resources/dataplex_metadata_sql_expected.sql
+++ tests/resources/dataplex_metadata_sql_expected.sql
@@ -15,12 +15,12 @@
 WITH
 zero_record AS (
 SELECT
-'<rule_binding_id>' AS rule_binding_id,
+'<rule_binding_id>' AS _internal_rule_binding_id,
 ),
 data AS (
 SELECT
 *,
-'<rule_binding_id>' AS rule_binding_id,
+'<rule_binding_id>' AS _internal_rule_binding_id,
 FROM
 `<your-gcp-project-id>.<your_bigquery_dataset_id>.contact_details` d
 WHERE
@@ -28,27 +28,27 @@ contact_type = 'email'
 ),
 last_mod AS (
 SELECT
-project_id || '.' || dataset_id || '.' || table_id AS table_id,
+project_id || '.' || dataset_id || '.' || table_id AS _internal_table_id,
 TIMESTAMP_MILLIS(last_modified_time) AS last_modified
 FROM `<your-gcp-project-id>.<your_bigquery_dataset_id>.__TABLES__`
 ),
 validation_results AS (
 SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NO_DUPLICATES_IN_COLUMN_GROUPS' AS rule_id,
-'<your-gcp-project-id>.<your_bigquery_dataset_id>.contact_details' AS table_id,
-CAST(NULL AS STRING) AS column_id,
-NULL AS column_value,
-CAST(NULL AS STRING) AS dimension,
-CAST(NULL AS BOOLEAN) AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-custom_sql_statement_validation_errors.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+CURRENT_TIMESTAMP() AS _internal_execution_ts,
+'<rule_binding_id>' AS _internal_rule_binding_id,
+'NO_DUPLICATES_IN_COLUMN_GROUPS' AS _internal_rule_id,
+'<your-gcp-project-id>.<your_bigquery_dataset_id>.contact_details' AS _internal_table_id,
+CAST(NULL AS STRING) AS _internal_column_id,
+NULL AS _internal_column_value,
+CAST(NULL AS STRING) AS _internal_dimension,
+CAST(NULL AS BOOLEAN) AS _internal_simple_rule_row_is_valid,
+TRUE AS _internal_skip_null_count,
+custom_sql_statement_validation_errors._internal_complex_rule_validation_errors_count AS _internal_complex_rule_validation_errors_count,
 CASE
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
-WHEN custom_sql_statement_validation_errors.complex_rule_validation_errors_count = 0 THEN TRUE
+WHEN custom_sql_statement_validation_errors._internal_complex_rule_validation_errors_count IS NULL THEN CAST(NULL AS BOOLEAN)
+WHEN custom_sql_statement_validation_errors._internal_complex_rule_validation_errors_count = 0 THEN TRUE
 ELSE FALSE
-END AS complex_rule_validation_success_flag,
+END AS _internal_complex_rule_validation_success_flag,
  r"""
  WITH
  zero_record AS (
@@ -134,7 +134,7 @@ _dq_validation_simple_rule_row_is_valid is False
 OR
 _dq_validation_complex_rule_validation_success_flag is False
 ORDER BY _dq_validation_rule_id"""
- AS failed_records_query,
+ AS _internal_failed_records_query,
  FROM
  zero_record
  LEFT JOIN
@@ -142,7 +142,7 @@ ORDER BY _dq_validation_rule_id"""
  SELECT
  *,
  '<rule_binding_id>' AS _rule_binding_id,
- COUNT(*) OVER() AS complex_rule_validation_errors_count,
+ COUNT(*) OVER() AS _internal_complex_rule_validation_errors_count,
  FROM (
  select a.*
  from data a
@@ -157,24 +157,24 @@ ORDER BY _dq_validation_rule_id"""
  ) custom_sql
  ) custom_sql_statement_validation_errors
  ON
- zero_record.rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id
+ zero_record._internal_rule_binding_id = custom_sql_statement_validation_errors._rule_binding_id
 UNION ALL
 SELECT
-CURRENT_TIMESTAMP() AS execution_ts,
-'<rule_binding_id>' AS rule_binding_id,
-'NOT_NULL_SIMPLE' AS rule_id,
-'<your-gcp-project-id>.<your_bigquery_dataset_id>.contact_details' AS table_id,
-'value' AS column_id,
-data.value AS column_value,
-CAST(NULL AS STRING) AS dimension,
+CURRENT_TIMESTAMP() AS _internal_execution_ts,
+'<rule_binding_id>' AS _internal_rule_binding_id,
+'NOT_NULL_SIMPLE' AS _internal_rule_id,
+'<your-gcp-project-id>.<your_bigquery_dataset_id>.contact_details' AS _internal_table_id,
+'value' AS _internal_column_id,
+data.value AS _internal_column_value,
+CAST(NULL AS STRING) AS _internal_dimension,
 CASE
 WHEN value IS NOT NULL THEN TRUE
 ELSE
 FALSE
-END AS simple_rule_row_is_valid,
-TRUE AS skip_null_count,
-CAST(NULL AS INT64) AS complex_rule_validation_errors_count,
-CAST(NULL AS BOOLEAN) AS complex_rule_validation_success_flag,
+END AS _internal_simple_rule_row_is_valid,
+TRUE AS _internal_skip_null_count,
+CAST(NULL AS INT64) AS _internal_complex_rule_validation_errors_count,
+CAST(NULL AS BOOLEAN) AS _internal_complex_rule_validation_success_flag,
  r"""
  WITH
  zero_record AS (
@@ -247,27 +247,27 @@ _dq_validation_simple_rule_row_is_valid is False
 OR
 _dq_validation_complex_rule_validation_success_flag is False
 ORDER BY _dq_validation_rule_id"""
- AS failed_records_query,
+ AS _internal_failed_records_query,
 FROM
 zero_record
 LEFT JOIN
 data
 ON
-zero_record.rule_binding_id = data.rule_binding_id
+zero_record._internal_rule_binding_id = data._internal_rule_binding_id
 ),
 all_validation_results AS (
 SELECT
-r.execution_ts AS execution_ts,
-r.rule_binding_id AS rule_binding_id,
-r.rule_id AS rule_id,
-r.table_id AS table_id,
-r.column_id AS column_id,
-r.column_value AS column_value,
-CAST(r.dimension AS STRING) AS dimension,
-r.skip_null_count AS skip_null_count,
-r.simple_rule_row_is_valid AS simple_rule_row_is_valid,
-r.complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
-r.complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
+r._internal_execution_ts AS execution_ts,
+r._internal_rule_binding_id AS rule_binding_id,
+r._internal_rule_id AS rule_id,
+r._internal_table_id AS table_id,
+r._internal_column_id AS column_id,
+r._internal_column_value AS column_value,
+CAST(r._internal_dimension AS STRING) AS dimension,
+r._internal_skip_null_count AS skip_null_count,
+r._internal_simple_rule_row_is_valid AS simple_rule_row_is_valid,
+r._internal_complex_rule_validation_errors_count AS complex_rule_validation_errors_count,
+r._internal_complex_rule_validation_success_flag AS complex_rule_validation_success_flag,
 (SELECT COUNT(*) FROM data) AS rows_validated,
 last_mod.last_modified,
 '{"brand": "one"}' AS metadata_json_string,
@@ -275,12 +275,12 @@ last_mod.last_modified,
 '<your_dataplex_lake_id>' AS dataplex_lake,
 '<your_dataplex_zone_id>' AS dataplex_zone,
 '<your_dataplex_asset_id>' AS dataplex_asset_id,
-CONCAT(r.rule_binding_id, '_', r.rule_id, '_', r.execution_ts, '_', True) AS dq_run_id,
+CONCAT(r._internal_rule_binding_id, '_', r._internal_rule_id, '_', r._internal_execution_ts, '_', True) AS dq_run_id,
 TRUE AS progress_watermark,
- failed_records_query AS failed_records_query,
+ _internal_failed_records_query AS failed_records_query,
 FROM
 validation_results r
-JOIN last_mod USING(table_id)
+JOIN last_mod USING(_internal_table_id)
 )
 SELECT
 *
